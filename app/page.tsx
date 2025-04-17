aa303ec73c55aaa862781d016877fb055df705c9
mini <projet>
2025-04-17 13:13:04 +0200

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import { Hero } from "@/components/Hero";
-import Image from "next/image";
+import { FloatingDockDemo } from "@/components/ui/home/FloatingDockDemo";
+import React from 'react';
 
-export default function Home() {
+const App = () => {
+  const handleClick = () => {
+    console.log("Card clicked!");
+  };
+
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col 
-    overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Hero ></Hero>
-      </div>
-    </main>
+    <div className="w-full h-screen overflow-x-hidden bg-gradient-to-l from-[#1800CD] to-[#100028]">
+  <FloatingDockDemo />
+</div>
+
   );
-}
+};
+
+export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {
-      colors: {
-        background: "#f0f0f0",
-        foreground: "#333",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
